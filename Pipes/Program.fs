64ed52f92f89af04e1a976215da49a7e4983f456
Stelios Georgiou <steliosdg@outlook.com>
2020-05-23 18:59:28 +0100

    Add project files.

---
+++ Pipes/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let square x = x * x
+
+let addOne x = x + 1
+
+let isOdd x = x % 2 <> 0
+
+let testProcess values = 
+    values
+    |> List.filter isOdd
+    |> List.map square
+    |> List.map addOne
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
+
+
