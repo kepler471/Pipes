ac5f62bc9106d1e9901c46c50550a5a15306334b
Stelios Georgiou <steliosdg@outlook.com>
2020-05-24 03:39:51 +0100

    testing out some basic features

--- Pipes/Program.fs
+++ Pipes/Program.fs
@@ -1,22 +1,32 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-let square x = x * x
+/// Initisalising random number generator
+let rand = System.Random()
+let randn = rand.Next()
 
-let addOne x = x + 1
+let dimX = 100
+let dimY = dimX
 
-let isOdd x = x % 2 <> 0
+let startingPosition = [ rand.Next(1,100), rand.Next(1,100) ]
 
-let testProcess values = 
-    values
-    |> List.filter isOdd
-    |> List.map square
-    |> List.map addOne
+let stepDirection position = position + rand.Next(0,1)
+
+let stepLength = 0
+
+// Infinite sequence random walk
+let rec endlessWalk x = 
+    seq { yield x
+          yield! endlessWalk (x + rand.NextDouble() - 0.5) }
+
+let X = [| 1 .. dimX |]
+let Y = [| 1 .. dimX |]
 
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+    printfn "%A" startingPosition
+    printfn "%A" (stepDirection 5)
+    // testing anonymouse functions on lists
+    printfn "%A" ( [1;2;3] |> List.map (fun x -> x + rand.Next(-1,1)))
     0 // return an integer exit code
-
-
