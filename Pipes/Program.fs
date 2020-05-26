036eea6c601d90a6efc8419c630146b861e4926e
Stelios Georgiou <steliosdg@outlook.com>
2020-05-26 02:03:49 +0100

    Random step function added for an n-dim vector, steps on all dims

--- Pipes/Program.fs
+++ Pipes/Program.fs
@@ -1,7 +1,7 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-// Initisalising random number generator
+/// Initisalising random number generator
 let rand = System.Random()
 let randn = rand.Next()
 
@@ -11,15 +11,22 @@ let walkLength = 10
 
 let startingPosition = [ rand.Next(1,dimX); rand.Next(1,dimY) ]
 
-// Randomly choose the change in direction in a given axis
-let stepDirection position = position + rand.Next(-1,1)
+/// Randomly choose the change in direction in a given axis
+let stepDirection position = position + rand.Next(-1,2)
 
-// Randomly choose the length of step
+/// Takes stepDirection function for single arg random step
+/// and maps the function for each item in the value position.
+/// This should work for integer, and list.
+let stepNDims (position: 'T list) = List.map stepDirection position
+
+/// Randomly choose the length of step
 let stepLength = rand.Next(1,2)
 
-// Choose a random direction
+/// Choose a random direction
+// This could take 2 directions for each dim in position vector
+// Could also use List.filter to drop occupied position from the options 
 let chooseDir position = 
-    let randMove = rand.Next(1,4)
+    let randMove = rand.Next(1,5)
     match randMove with
         | 1 -> "a"
         | 2 -> "b"
@@ -27,12 +34,13 @@ let chooseDir position =
         | 4 -> "d"
         |_ -> position
 
-// Infinite sequence random walk
+
+/// Infinite sequence random walk
 let rec endlessWalk x = 
     seq { yield x
           yield! endlessWalk (x + rand.NextDouble() - 0.5) }
 
-// Infinite sequence random walk, piped
+/// Infinite sequence random walk, piped
 let rec endlessWalkPipe x = 
     seq { yield x
           yield! 
@@ -40,22 +48,18 @@ let rec endlessWalkPipe x =
             |> endlessWalkPipe }
 
 
-// Infinite sequence random walk in 2-dims
+/// Infinite sequence random walk in 2-dims
 let rec endlessWalkInt [x;y] = 
     seq { yield [x;y]
           yield! endlessWalkInt [x+1;y+1] }
 
-let step x = x+1
-
-let stepPair x = x
 
 // The function above is very specific to a pair/list input
-// Take a generic function to operate on the next step of each recursion
+/// Take a generic function to operate on the next step of each recursion
 let rec endlessWalkIntGen x = 
     seq { yield x
           yield! 
             x 
-            /// step function 
             |> step 
             |> endlessWalkIntGen }  
 
@@ -63,7 +67,7 @@ let rec endlessWalkIntGen x =
 let X = [ 1 .. dimX ]
 let Y = [ 1 .. dimY ]
 
-// Space by nested sequences
+/// Space by nested sequences
 let SpaceNestedSeq = [ for x in 1 .. dimX 
     -> [for y in 1 .. dimY 
     -> [x,y]]]
@@ -83,7 +87,7 @@ let SpaceNestedSeq = [ for x in 1 .. dimX
 [<EntryPoint>]
 let main argv =
     //printfn "%A" startingPosition
-    //printfn "%A" (stepDirection 5)
+    printfn "%A" (stepDirection 5)
     // testing anonymouse functions on lists
     //printfn "%A" ( [1;2;3] |> List.map (fun x -> x + rand.Next(-1,1)) )
     //printfn "%A" ( [1..10] |> List.map (fun x -> [1..10]) )
